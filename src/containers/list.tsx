36e67caa8a57d6cc05639f68794a717d2d068465
christian <baozaodexianyu@163.com>
2019-01-16 19:59:48 +0800

    add: list.tsx

---
+++ src/containers/list.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+export default class List extends React.Component <{}> {
+    constructor(props: {}){
+      super(props)
+    }
+
+    public render() {
+      return (
+        <div>
+            1234 list
+        </div>
+      )
+    }
+}
